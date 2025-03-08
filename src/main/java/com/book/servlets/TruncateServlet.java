97bc34f786aa4dd7a9411a97be12324cd9e2ac39
santhosh kumar <168417389+SunthoshNannuri@users.noreply.github.com>
2025-03-08 23:25:56 +0530

    Update TruncateServlet.java

--- src/main/java/com/book/servlets/TruncateServlet.java
+++ src/main/java/com/book/servlets/TruncateServlet.java
@@ -12,8 +12,27 @@ public class TruncateServlet extends HttpServlet {
 
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
 	{
+		// Fetch from Render Environment Variables
+        String urlsql = System.getenv("DB_URL"); 
+        String usernamesql = System.getenv("DB_USERNAME");
+        String passwordsql = System.getenv("DB_PASSWORD");
+
+        // Fallback for Local Testing
+        if (urlsql == null || urlsql.isEmpty()) {
+            ServletContext ctx = getServletContext();
+            urlsql = ctx.getInitParameter("url");  // Read from web.xml
+            usernamesql = ctx.getInitParameter("username");
+            passwordsql = ctx.getInitParameter("password");
+        }
+
+        // Store in Object
+        Dbdetails db = new Dbdetails();
+        db.setUrl(urlsql);
+        db.setUsername(usernamesql);
+        db.setPassword(passwordsql);
+
 		UserRepository user=new UserRepository();
-		String response=user.truncate();
+		String response=user.truncate(db);
 		System.out.println(response);
 		RequestDispatcher dispatch=req.getRequestDispatcher("vistagain.html");
 		dispatch.forward(req, resp);
